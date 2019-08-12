744ce3d1e74de802ee50cf6a6f05c6c3d066b5e2
GuichengIT <zhangguicheng_1995@163.com>
2019-08-12 12:15:50 +0800

    update

--- 轮播图/js/script.js
+++ 轮播图/js/script.js
@@ -1,12 +1,18 @@
 /*声明全局变量*/
 var index=0,//当前显示图片的索引，默认值为0
     d = 0,//当前圆点的索引
-    timer = 0;
+    timer = null;//定时器，开启定时器后变为非空值
+    main = byId("main"),
     prev = byId("prev"),
     next = byId("next"),
     pics = byId("banner").getElementsByTagName("div"),
     dot = byId("dots"),
-    dots = dot.getElementsByTagName("span")
+    innerBox =  byId("inner-box"),
+    menuContent = byId("menu-content");
+    subMenu = byId("sub-menu"),
+    menuItems = menuContent.getElementsByClassName("menu-item"),
+    dots = dot.getElementsByTagName("span"),
+    subInnerBoxs = innerBox.getElementsByClassName("sub-inner-box"),
     pics_cnt = pics.length;//图片的数量
     dots_cnt = dots.length;//小圆点的数量
 
@@ -26,6 +32,7 @@ function byId(id) {
     return typeof(id) === "string" ? document.getElementById(id):id;
 }
 
+
 // 自动轮播
 function startAutoPlay() {
     timer = setInterval(function(){
@@ -36,6 +43,13 @@ function startAutoPlay() {
         switch_pic();
         console.log(index);
     },3000)
+}
+
+//停止自动轮播
+function stopAutoPlay(){
+    if(timer){
+        clearInterval(timer);
+    }
 }
 
 /*
@@ -87,6 +101,32 @@ addHandler(dot, "click", function (){
     }
 });
 
+//鼠标滑过主菜单显示子菜单(mouseover和mouseout事件不适合利用事件委托)
+for(var i=0;i<4;i++){
+addHandler(menuItems[i], "mouseover", function(event){
+    var target = event.target;
+    console.log(target);
+    subInnerBoxs[1].style.display = "block";
+    subMenu.className = "sub-menu";
+});
+}
+//鼠标滑出主菜单隐藏子菜单（利用事件委托）
+
+// addHandler(menuContent,"mouseout",function(event){
+//     for(var i=0,menuItemLen = menuItem.length;i<menuItemLen;i++){
+//         if(event.target === menuItem[i]){
+//             subMenu.className = "sub-menu hide";
+//             subInnerBoxs[i].style.display = "block";
+//         }
+//     }
+// });
+
+
+
+//鼠标滑入main，停止轮播
+addHandler(main,"mouseover",stopAutoPlay);
+//鼠标离开main，继续轮播
+addHandler(main,"mouseout",startAutoPlay);
 //自动开始轮播
 startAutoPlay();
 
